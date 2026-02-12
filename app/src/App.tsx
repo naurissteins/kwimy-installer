a097358128d2738ac37507cf3c579fa670c73e55
naurissteins <me@naurissteins.com>
2026-02-12 09:39:55 +0200

    first commit

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-dark-2">
+      <h1 className="text-5xl font-bold text-purple-200">
+        Welcome to Kwimy Installer!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
