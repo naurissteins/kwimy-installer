1185ad40b23464e04dfdd136fdf0063938a1a337
naurissteins <me@naurissteins.com>
2026-02-12 09:58:53 +0200

    Added new Welcome component and applied the routing change in App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,8 @@
 import "./App.css";
+import Welcome from "./steps/welcome/Welcome";
 
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-dark-2">
-      <h1 className="text-5xl font-bold text-purple-200">
-        Welcome to Kwimy Installer!
-      </h1>
-    </div>
-  );
+  return <Welcome />;
 }
 
 export default App;
---
+++ app/src/steps/welcome/Welcome.tsx
@@ -0,0 +1,11 @@
+function Welcome() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-dark-2">
+      <h1 className="text-5xl font-bold text-purple-200">
+        Welcome to Kwimy Installer! Nice!
+      </h1>
+    </div>
+  );
+}
+
+export default Welcome;
